938070196dc69f114403779c07f3f35639ca0a41
Maria Prylutska <ma7i7iq1q1q1@gmail.com>
2023-08-23 16:03:44 +0300

    added currency model

---
+++ Coins/MVVM/Models/Currency.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coins.MVVM.Models
+{
+    public class Currency
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string ImageURL { get; set; }
+        public decimal Price { get; set; }
+        public decimal Volume { get; set; }
+        public decimal PriceChange { get; set; }
+        public List<Market> Markets { get; set; }
+    }
+}
