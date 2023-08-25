47c30d76c19103c5841f470ab09715224a738418
Maria Prylutska <ma7i7iq1q1q1@gmail.com>
2023-08-26 00:18:30 +0300

    updated market model

---
+++ Coins/MVVM/Models/CurrencyMarketInfo.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+
+namespace Coins.MVVM.Models
+{
+    public class CurrencyMarketInfo
+    {
+        [JsonProperty("exchangeId")]
+        public string ExchangeId { get; set; }
+
+        [JsonProperty("baseId")]
+        public string BaseId { get; set; }
+
+        [JsonProperty("quoteId")]
+        public string QuoteId { get; set; }
+
+        [JsonProperty("baseSymbol")]
+        public string BaseSymbol { get; set; }
+
+        [JsonProperty("quoteSymbol")]
+        public string QuoteSymbol { get; set; }
+
+        [JsonProperty("volumeUsd24Hr")]
+        public string VolumeUsd24Hr { get; set; }
+
+        [JsonProperty("priceUsd")]
+        public string PriceUsd { get; set; }
+
+        [JsonProperty("volumePercent")]
+        public string VolumePercent { get; set; }
+    }
+}
