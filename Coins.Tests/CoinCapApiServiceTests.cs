115eadfafa36384fecd8f69517073be64dba1d68
Maria Prylutska <ma7i7iq1q1q1@gmail.com>
2023-08-25 17:13:52 +0300

    added CoinCapApiService tests

---
+++ Coins.Tests/CoinCapApiServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using Coins.Services;
+
+namespace Coins.Tests
+{
+    [TestFixture]
+    public class CoinCapApiServiceTests
+    {
+        [Test]
+        public async Task GetTopCurrenciesAsync_WhenCalled_ReturnsListOfCurrencies()
+        {
+            var service = new CoinCapApiService();
+
+            var result = await service.GetTopCurrenciesAsync();
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result[0].Id, Is.EqualTo("bitcoin"));
+        }
+    }
+}
