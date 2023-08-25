bf3eae6cfb3e5adce8c05995ea4d37b57694fc91
Maria Prylutska <ma7i7iq1q1q1@gmail.com>
2023-08-25 17:12:32 +0300

    replaced CoinGecko service with CoinCap

---
+++ Coins/Services/CoinCapApiService.cs
@@ -0,0 +1,47 @@
+﻿using Coins.MVVM.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Coins.Services
+{
+    public class CoinCapApiService
+    {
+        private const string BaseUrl = "https://api.coincap.io/v2/";
+
+        private readonly HttpClient _httpClient;
+
+        public CoinCapApiService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<List<Currency>> GetTopCurrenciesAsync()
+        {
+            var url = $"{BaseUrl}assets"; // Corrected URL to fetch assets
+            var response = await _httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+
+                // The API response structure has a nested 'data' property containing an array of currencies
+                var apiResponse = JsonConvert.DeserializeObject<ApiResponse>(content);
+                var currencies = apiResponse.Data;
+
+                return currencies;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+
+    // Define the ApiResponse class to match the structure of the API response
+    public class ApiResponse
+    {
+        public List<Currency> Data { get; set; }
+    }
+}
